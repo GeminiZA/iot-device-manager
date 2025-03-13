bd06221dd667826e30b117a4f463c4307cc34fc5
Tristan Theron <tristantheron@gmail.com>
2025-03-13 15:40:12 +0200

    Removed unused postgres function and updated .env.example

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Building with sqlite so long, implementing postgres should be easy
 	sqliteDb, err := database.ConnectSqlite(cfg.SqlitePath)
 	if err != nil {
 		log.Fatal(err)
